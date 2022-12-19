eae7b9cd5e8473e75ab7537c2ee6dc153e721829
s4llm4n <s4llm4n@gmail.com>
2022-12-19 21:48:04 +0700

    commit detail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       
       {/* AOS Animation */} 
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
       
       {/* Call Bootstrap JS */}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
