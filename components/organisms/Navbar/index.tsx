2a810aa1798b47a3f9ab817f4df1dc2114e4cee3
s4llm4n <s4llm4n@gmail.com>
2022-12-23 19:25:40 +0700

    commit auth

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Menu from './Menu';
+import Auth from './Auth';
 
 export default function Navbar() {
   return (
@@ -27,37 +28,11 @@ export default function Navbar() {
                 <Menu title="Reward" />
                 <Menu title="Discover" />
                 <Menu title="Global Rank" />
-                <li className="nav-item my-auto">
-                  <a
-                    className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill"
-                    href="./src/sign-in.html"
-                    role="button"
-                  >
-                    Sign In
-                  </a>
-                </li>
-                <li className="nav-item my-auto dropdown d-flex">
-                            <div className="vertical-line d-lg-block d-none"></div>
-                            <div>
-                                <a className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="/img/avatar-1.png" className="rounded-circle" width="40" height="40"
-                                        alt=""/>
-                                </a>
-
-                                <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">My Profile</a></li>
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">Wallet</a></li>
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">Account Settings</a>
-                                    </li>
-                                    <li><a className="dropdown-item text-lg color-palette-2" href="#">Log Out</a></li>
-                                </ul>
-                            </div>
-                        </li>
+              <Auth />
               </ul>
             </div>
           </div>
         </nav>
       </section>
-  )
+  );
 }
